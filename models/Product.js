50e35976c64c837deb3ae54602cd8e7a129a856c
clacson95 <caleb.lacson@gmail.com>
2022-12-20 21:43:39 -0500

    finished models

--- models/Product.js
+++ models/Product.js
@@ -14,17 +14,17 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
-        isDecimal: true
+        isDecimal: true,
       }
     },
     stock: {
@@ -32,14 +32,14 @@ Product.init(
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumeric: true
+        isNumeric: true,
       }
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "category",
-        key: "id"
+        key: "id",
       }
     },
   },
